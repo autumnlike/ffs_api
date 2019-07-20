0f31e3de4a796c323461acf50974d6c74525e751
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2019-07-21 02:07:36 +0900

    feat: ユーザー一覧にストレス値を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_one :user_ffs, class_name: "UserFFS"
+  has_many :user_stresses, -> { order('diagnostic_at DESC') }
 
   def self.create_by_ethos!(data)
     u = new_by_ethos(data)
