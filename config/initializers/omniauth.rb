980c5be98a2e8dc2e8998134ee2fdd0bc48c3120
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2019-07-28 12:37:45 +0900

    fix: ログイン保持する内容を増やしてアイコンとか使う

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #options = { prompt: 'consent', hd: '@lancers.co.jp', name: 'google', scope: %w(email)  }
 	# FIXME ドメインを動的に指定したい
 	# hd: ドメイン指定
-  options = { prompt: 'consent', hd: ENV['GOOGLE_CLIENT_DOMAIN'], name: 'google', scope: %w(email)  }
+  options = { prompt: 'consent', hd: ENV['GOOGLE_CLIENT_DOMAIN'], name: 'google', scope: %w(email userinfo.profile), skip_jwt: true }
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], options
 end
