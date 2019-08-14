0bac7c88b8e6bf573fdb9b648aeac3ee2a9c081f
autumnlike <akiyoshi.satoshi@lancers.co.jp>
2019-08-14 13:39:25 +0900

    fix: メンバー詳細ページ追加

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,20 @@
 class MembersController < ApplicationController
+  before_action :set_member, only: [:show]
+
   def index
     @members = Member.all
   end
+
+  # GET /members/1
+  # GET /members/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_member
+      @member = Member.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      redirect_to root_path
+    end
 end
